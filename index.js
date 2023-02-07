1d65c31869a13df68dc90a15595a45cd573712eb
Holly <h.person.notrobot@gmail.com>
2023-02-07 22:34:15 +0000

    Pet image selection

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const pets = {
+    "cat 1": "https://stardewvalleywiki.com/mediawiki/images/thumb/1/1e/Cat_1.png/48px-Cat_1.png",
+    "cat 2": "https://stardewvalleywiki.com/mediawiki/images/thumb/8/83/Cat_2.png/48px-Cat_2.png",
+    "cat 3": "https://stardewvalleywiki.com/mediawiki/images/thumb/e/ec/Cat_3.png/48px-Cat_3.png",
+    "dog 1": "https://stardewvalleywiki.com/mediawiki/images/thumb/a/a4/Dog_1.png/48px-Dog_1.png",
+    "dog 2": "https://stardewvalleywiki.com/mediawiki/images/thumb/b/bb/Dog_2.png/48px-Dog_2.png",
+    "dog 3": "https://stardewvalleywiki.com/mediawiki/images/thumb/f/fc/Dog_3.png/48px-Dog_3.png",
+    "horse": "https://stardewvalleywiki.com/mediawiki/images/c/c3/Horse.png"
+}
+
 function copy() {
     html2canvas(document.querySelector("#main")).then(canvas => {
         canvas.toBlob(function (blob) {
@@ -23,6 +33,7 @@ function dosubmit (event) {
     let name = document.getElementById("name_input").value;
     let bio = document.getElementById("bio_input").value;
     let pet = document.getElementById("pet_input").value;
+    let pet_type = document.getElementById("pettype_input").value;
     let horse = document.getElementById("horse_input").value;
     let birthmonth = document.getElementById("birthmonth_input").value;
     let birthday = document.getElementById("birthday_input").value;
@@ -38,6 +49,7 @@ function dosubmit (event) {
     document.getElementById("season").innerHTML = birthmonth
     document.getElementById("day0").innerHTML = Math.floor(birthday/10).toString()
     document.getElementById("day1").innerHTML = (birthday-(Math.floor(birthday/10)*10)).toString()
+    document.getElementById("pet_img").src = pets[pet_type]
 
     // document.getElementById("name").innerHTML = name
 
@@ -49,12 +61,3 @@ function dosubmit (event) {
 
 
 
-const pets = {
-    "cat 1": "https://stardewvalleywiki.com/mediawiki/images/thumb/1/1e/Cat_1.png/48px-Cat_1.png",
-    "cat 2": "https://stardewvalleywiki.com/mediawiki/images/thumb/8/83/Cat_2.png/48px-Cat_2.png",
-    "cat 3": "https://stardewvalleywiki.com/mediawiki/images/thumb/e/ec/Cat_3.png/48px-Cat_3.png",
-    "dog 1": "https://stardewvalleywiki.com/mediawiki/images/thumb/a/a4/Dog_1.png/48px-Dog_1.png",
-    "dog 2": "https://stardewvalleywiki.com/mediawiki/images/thumb/b/bb/Dog_2.png/48px-Dog_2.png",
-    "dog 3": "https://stardewvalleywiki.com/mediawiki/images/thumb/f/fc/Dog_3.png/48px-Dog_3.png",
-    "horse": "https://stardewvalleywiki.com/mediawiki/images/c/c3/Horse.png"
-}
